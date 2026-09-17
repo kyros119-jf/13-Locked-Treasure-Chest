f68a961d31be34bc23a6c71ee4b21f82cb4624e7
Jeremy <jeremy.frank@students.snow.edu>
2026-09-17 00:13:12 -0600

    Complete locked treasure chest assignment

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿TreasureChest chest = new TreasureChest();
+chest.AddGold(100);
+chest.RemoveGold(20);
+chest.RemoveGold(40);
+;
+
+
+Console.WriteLine($"Current gold: { chest.ViewGold()}");
---
+++ TreasureChest.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Security.Cryptography.X509Certificates;
+
+public class TreasureChest
+{
+    private int gold;
+
+
+public void AddGold(int amount)
+{
+    if(amount > 0)
+    gold += amount;
+    Console.WriteLine($"Gold added: {amount}");
+
+    }
+
+    public void RemoveGold(int amount)
+    {
+        if (amount <= gold)
+        {
+            gold -= amount;
+            Console.WriteLine($"Gold removed: {amount}");
+
+        }
+        else
+        {
+            Console.WriteLine("Cannot remove more gold than the chest contains.");
+        }
+    }
+public int ViewGold()
+{
+    return gold;
+}
+  }
